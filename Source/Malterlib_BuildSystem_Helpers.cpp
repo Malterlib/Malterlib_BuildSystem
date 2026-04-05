dd388140c458148e7f711213daaf82640a1b3855
Erik Olofsson <erik@olofsson.info>
2026-04-05 20:33:37 +0200

    Update licensing to Apache 2.0 with LLVM Exceptions

--- Source/Malterlib_BuildSystem_Helpers.cpp
+++ Source/Malterlib_BuildSystem_Helpers.cpp
@@ -1,5 +1,5 @@
-// Copyright © 2015 Hansoft AB
-// Distributed under the MIT license, see license text in LICENSE.Malterlib
+// Copyright © Unbroken AB
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include "Malterlib_BuildSystem.h"
 #include "Malterlib_BuildSystem_Helpers.h"
