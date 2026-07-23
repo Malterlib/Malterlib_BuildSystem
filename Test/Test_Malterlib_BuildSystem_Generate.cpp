002fff1bae233c7a870a2382c214e552c08b0c6d
Erik Olofsson <erik@olofsson.info>
2026-07-23 17:58:21 +0200

    Fix compile error

--- Test/Test_Malterlib_BuildSystem_Generate.cpp
+++ Test/Test_Malterlib_BuildSystem_Generate.cpp
@@ -36,6 +36,11 @@ namespace
 			co_return {};
 		}
 
+		TCFuture<TCActorSubscriptionWithID<>> f_RegisterForScreenChange(FOnScreenChange _fOnScreenChange) override
+		{
+			co_return {};
+		}
+
 		TCFuture<NContainer::CIOByteVector> f_ReadBinary() override
 		{
 			co_return {};
